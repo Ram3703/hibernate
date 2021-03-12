3dc2e73462bc7234e37898675ba477a389d49b02
Ram3703 <78849459+Ram3703@users.noreply.github.com>
2021-03-12 13:24:28 +0530

    commit

---
+++ hibernate/src/main/java/com/ltts/hibernate/App.java
@@ -0,0 +1,31 @@
+package com.ltts.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      Configuration cfg=new Configuration();
+      cfg.configure("hibernate.cfg.xml");
+      SessionFactory factory=cfg.buildSessionFactory();
+      Session session=factory.openSession();
+      Transaction t=session.beginTransaction();
+      Employee e=new Employee();
+      e.setEmpid(101);
+      e.setEmpname("Teja");
+      e.setEmpaddress("Mysore");
+     
+      session.save(e);
+      t.commit();
+      factory.close();
+      session.close();
+    }
+}
